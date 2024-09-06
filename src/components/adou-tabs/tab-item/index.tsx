8c5a9e773a882fb817fd1515c60c7ae7e1d19ea6
adouaa <1041137820@qq.com>
2024-09-06 21:13:43 +0800

    美化了Tabs的样式，新增contentPadding属性

--- src/components/adou-tabs/tab-item/index.tsx
+++ src/components/adou-tabs/tab-item/index.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { withTranslation } from "react-i18next"
+import React from 'react';
+import { withTranslation } from 'react-i18next';
 
 interface TabItemProps {
-    label?: string,
-    children?: any,
-    active?: boolean,
-    prefixIcon?: string,
-    activeLabelColor?: string
+    label?: string;
+    children?: any;
+    active?: boolean;
+    contentPadding?: string;
 }
 
 const TabItem = (props: TabItemProps) => {
-
-    const { label, children, active, activeLabelColor = "#409eff" } = props;
-    return <>
-        {active && <div className="tab-item-wrapper" style={{padding: "15px 10px"}}>{children}</div>}
-    </>
-}
+    const { label, children, active, contentPadding } = props;
+    return <>{active && <div style={{ padding: contentPadding || '0px 10px' }}>{children}</div>}</>;
+};
 
 export default withTranslation()(TabItem);
